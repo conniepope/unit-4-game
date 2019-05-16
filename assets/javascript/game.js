84c740a559d429e2e25fb59503291117926d3217
conniepope <conniepope77@gmail.com>
2019-05-16 19:17:04 -0400

    basic CSS added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,7 +19,7 @@ var randomNumber = Math.floor(Math.random() * (max - min +1)) + min;
 console.log(randomNumber) 
 
 var updateScore = function() {
-    $("#score").html("Your number: " + score);
+    $("#score").html(score);
 }
 
 //RESET
@@ -32,7 +32,7 @@ var reset = function() {
     console.log(randomNumber)
 
     // the random number is diplayed
-    $(".random-number").text("Your numbert to match: " + randomNumber)
+    $(".random-number").text(randomNumber)
 
     // each crystal gets a random value between 1 - 12 
     crystal1 = Math.floor(Math.random() * (cMax - cMin +1)) + cMin;
@@ -47,24 +47,24 @@ reset();
 $("#crystal-image1").on("click", function() {
     // score = score + crystal1;
     score += crystal1;   
-    $("#score").html("Your number: " + score);
+    $("#score").html(score);
     console.log(crystal1)
     console.log(score)
 });
 
 $("#crystal-image2").on("click", function() {
-    score += crystal2;   
-    $("#score").html("Your number: " + score);
+    score += crystal2;
+    $("#score").html(score);
 });
 
 $("#crystal-image3").on("click", function() {
     score += crystal3;   
-    $("#score").html("Your number: " + score);
+    $("#score").html(score);
 });
 
 $("#crystal-image4").on("click", function() {
     score += crystal4;   
-    $("#score").html("Your number: " + score);
+    $("#score").html(score);
 });
 
 
@@ -72,7 +72,7 @@ $(".crystal-container").on("click", function() {
     if (score > randomNumber) {
         //    * The player loses if their score goes above the random number.
         losses++;
-        $("#losses").text("Losses: " + losses);
+        $("#losses").text(losses);
         alert("Sorry, you lost. Try again.")
         console.log("loser");
         reset()
@@ -80,7 +80,7 @@ $(".crystal-container").on("click", function() {
     } else if (score === randomNumber) {
         //    * The player wins if their total score matches the random number from the beginning of the game.
         wins++;
-        $("#wins").text("Wins:  " + wins);
+        $("#wins").text(wins);
         alert("YOU WON!!!");
         console.log("winner");
         reset()
